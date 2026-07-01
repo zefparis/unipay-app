a33096f9df12c1e7d38528a84fed9f2644693010
zefparis <ben.barere@gmail.com>
2026-07-01 22:26:36 +0200

    feat: enable locale detection

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,5 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['fr', 'en'],
   defaultLocale: 'fr',
-  localeDetection: false,
+  localeDetection: true,
 });
